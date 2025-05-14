cee8a2840cd93976dc31682301a94acd2e9c2446
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-14 23:03:18 +0900

    Adding LazyColumn and Data Classes

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val shoppingItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
@@ -34,9 +41,25 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text(text = "Add Item")
                         }
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            items(shoppingItems) {
+
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false,
+)
