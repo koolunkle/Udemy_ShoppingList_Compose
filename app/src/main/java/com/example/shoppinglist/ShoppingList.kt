8b1bce350d674d967789c632895316eee022ab50
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-16 21:53:09 +0900

    Change shopping items

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,5 +1,6 @@
 package com.example.shoppinglist
 
+import android.content.Context
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -18,6 +19,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -33,16 +35,25 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 data class ShoppingItem(
     val id: Int,
     var name: String,
     var quantity: Int,
     var isEditing: Boolean = false,
+    var address: String = "",
 )
 
 @Composable
-fun ShoppingListApp(modifier: Modifier = Modifier) {
+fun ShoppingListApp(
+    locationUtils: LocationUtils,
+    viewModel: LocationViewModel,
+    navController: NavController,
+    context: Context,
+    address: String,
+    modifier: Modifier = Modifier
+) {
     var shoppingItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var isShowDialog by remember { mutableStateOf(false) }
 
@@ -210,14 +221,29 @@ fun ShoppingListItem(
             ),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(
-            text = "Name: ${item.name}",
-            modifier = Modifier.padding(8.dp),
-        )
-        Text(
-            text = "Quantity: ${item.quantity}",
-            modifier = Modifier.padding(8.dp),
-        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp)
+        ) {
+            Row {
+                Text(
+                    text = "Name: ${item.name}",
+                    modifier = Modifier.padding(8.dp),
+                )
+                Text(
+                    text = "Quantity: ${item.quantity}",
+                    modifier = Modifier.padding(8.dp),
+                )
+            }
+            Row(modifier = Modifier.fillMaxSize()) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = null,
+                )
+                Text(text = item.address)
+            }
+        }
         Row(modifier = Modifier.padding(8.dp)) {
             IconButton(onClick = onEditClick) {
                 Icon(
